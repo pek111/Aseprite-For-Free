64c909f13abf36bc2b868faac12cc0a94e5b3ac0
pek111 <143720861+pek111@users.noreply.github.com>
2024-06-30 08:38:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function downloadAseprite() {
-    // Redirect to the download page
+    // Redirect to the download link
+    window.location.href = "https://github.com/pek111/Aseprite-For-Free/releases/download/main/Aseprite-v1.3.7.zip";
+}
+
+function toGitHub() {
+    // Redirect to the GitHub page
     window.location.href = "https://github.com/pek111/Aseprite-For-Free/releases/tag/main";
 }
